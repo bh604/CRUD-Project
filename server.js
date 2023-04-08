a0597c29160095a09fe8fc24d4b3073f0834beff
Brian <bhoangster@live.ca>
2023-04-07 22:32:47 -0700

    made some changes

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ MongoClient.connect(connectionString)
     app.set('view engine', 'ejs')
     app.use(bodyParser.urlencoded({ extended: true }))
     app.use(express.static('public'))
+    app.use(bodyParser.json())
     app.get('/', (req, res) => {
       quotesCollection.find().toArray()
         .then(results => {
@@ -24,6 +25,36 @@ MongoClient.connect(connectionString)
       quotesCollection.insertOne(req.body)
         .then(result => {
           res.redirect('/')
+        })
+        .catch(error => console.error(error))
+    })
+    app.put('/quotes', (req, res) => {
+      quotesCollection.findOneAndUpdate(
+        { name: 'Yoda' },
+        {
+          $set: {
+            name: req.body.name,
+            quote: req.body.quote
+          }
+        },
+        {
+          upsert: true
+        }
+      )
+      .then(result => {
+        res.json('Success')
+      })
+      .catch(error => console.error(error))
+    })
+    app.delete('/quotes', (req, res) => {
+      quotesCollection.deleteOne(
+        { name: req.body.name },
+      )
+        .then(result => {
+          if (result.deletedCount === 0) {
+            return res.json(`No quote to delete`)
+          }
+          res.json(`Delete Darth Vader's quote`)
         })
         .catch(error => console.error(error))
     })
